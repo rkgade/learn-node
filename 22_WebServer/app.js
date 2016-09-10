e6e9afe57e4b16ccd21222f39d518f16f79dd031
Raj Kiran Gade <rajkiran.gade@gmail.com>
2016-09-10 12:56:23 +0530

    modified with templates

--- 22_WebServer/app.js
+++ 22_WebServer/app.js
@@ -1,11 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function(req, res) {
   // write Head : first argument is status code.
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  console.log(req);
-  res.write('How are you?');
-  res.end('Hello World \n');
-  console.log(res);
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+// read the index.htm file.
+// with utf8, we get the string in html and not binary data.
+  var html = fs.readFileSync(__dirname+ '/index.htm','utf8');
+  var message = 'Hello!';
+  // templates
+  html = html.replace('{Message}',message);
+  console.log(html);
+//  res.write('How are you?');
+  res.end(`${html}`);
+
 
 }).listen(3001, 'localhost');
