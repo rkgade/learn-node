ad94f7bae11e17a48c592e86f1560089ea59dd5f
Raj Kiran Gade <rajkiran.gade@gmail.com>
2016-09-10 23:27:47 +0530

    updated express, send and receive json

--- 26_Express/app.js
+++ 26_Express/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 // app is a function that has properties and methods that require('express'); returns.
 var app = express();
 
 // Use Environment Variables.
-var port = process.env.PORT || 5000 ;
+var port = process.env.PORT || 2001 ;
+
+// body-parser urlencodedParser
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+//console.log(bodyParser);
+var jsonParser = bodyParser.json();
+console.log(jsonParser);
 
 // static files and middleware.
 // What should happen between request and response.
@@ -16,7 +24,7 @@ app.set('view engine', 'ejs')
 // own middleware
 // loggers can help here.
 app.use('/', function(req, res, next){
-  console.log('Response url'+ req.url);
+//  console.log('Response url'+ req.url);
   // this means, run the next middleware.
   next();
 });
@@ -29,13 +37,29 @@ app.get('/', function(req, res) {
 // : says that id can be anyting.
 // This also helps to poll data using URL.
 app.get('/person/:id', function(req, res) {
-
-    res.render('person',{ID : req.params.id});
+    // Now query object will be empty if there is no query string.
+    // the string mentioned after req.query should be present in the URL.
+    res.render('person',{ID : req.params.id, Qstr: req.query.qstring});
 });
+
+app.post('/person', urlencodedParser, function(req, res) {
+  res.send('Thank you!');
+  console.log(req.body);
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+  res.send('Thank you for the JSON Data');
+  console.log('Printing JSON values');
+  console.log(req.body);
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
 app.get('/api', function(req, res) {
   res.json({ firstname: 'Raj Kiran ', lastname: 'Gade'})
 });
-
 
 
 // listen just creates a listener.
